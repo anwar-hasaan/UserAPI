64fff304ecc747ca41a057034f279bd7ee038a44
anwar-hasaan <anwarhossene@gmail.com>
2022-11-09 22:21:28 +0600

    initial commit

---
+++ account/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+# Custom User Manager
+class UserManager(BaseUserManager):
+    def create_user(self, name, email, tc, password=None, password2=None):
+        #Creates and saves a User with the given name, email, tc and password.
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            name = name,
+            email=self.normalize_email(email),
+            tc = tc,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, name, email, tc, password=None):
+        #Creates and saves a superuser with the given name, email, tc and password.
+        user = self.create_user(
+            name=name,
+            email=email,
+            tc=tc,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+# Custom User Model
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(verbose_name='email', max_length=255, unique=True)
+    tc = models.BooleanField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'tc']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
